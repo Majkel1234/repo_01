278b0575ecfe78c74ea733de16d0afc666b5e7f4
Jurek <wwsis123362@wp.pl>
2016-10-28 20:27:13 +0200

    [Majkel1234] - 1st commit.]

---
+++ git_test_02/git_test_02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class X{
+public:
+	X(int xx = 0):x{xx}{}
+	X(const X &&T) = delete;
+	int x;
+};
+
+void fun( X &T){
+	cout<<"(jestem w funkcji)"<<endl;
+	T.x = 333;
+}
+
+
+int main(){
+	fun(X(123));
+
+	_getch();
+}
+
